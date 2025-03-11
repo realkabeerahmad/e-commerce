a647d23c1ff6414fc241d87de1b4953a7865c610
realkabeerahmad <kabeerahmadofficial@gmail.com>
2025-03-11 17:40:38 +0500

    Addition of methods on product model

--- server-app/src/controller/products.ts
+++ server-app/src/controller/products.ts
@@ -30,7 +30,7 @@ export const create = async (req: express.Request, res: express.Response) => {
 
 export const getAll = async (req: express.Request, res: express.Response) => {
   try {
-    const products = await productModel.find({});
+    const products = await productModel.find({}).populate("category", "name -_id").populate("store", "name -_id");
     if (products.length <= 0) throw new HttpError("NotFoundError", "No products found!", 404, null);
     sendResponse(res, 200, 'Products Fetched successfully!', products);
   } catch (error) {
@@ -38,21 +38,42 @@ export const getAll = async (req: express.Request, res: express.Response) => {
   }
 };
 
-export const getOne = (req: express.Request, res: express.Response) => {
-  let http_status = 200;
-  let message = "End point is working.";
+export const getOne = async (req: express.Request, res: express.Response) => {
   try {
-    sendResponse(res, http_status, message, null);
+    const { id } = req.params
+    const product = await productModel.findById({ _id: id }).populate("category", "name -_id").populate("store", "name -_id");
+    if (!product) throw new HttpError("NotFoundError", "Product not found!", 404, null)
+    sendResponse(res, 200, "Product Fetched Successfully", product);
   } catch (error) {
     handleError(error, res);
   }
 };
 
-export const updateOne = (req: express.Request, res: express.Response) => {
-  let http_status = 200;
-  let message = "End point is working.";
+export const updateOne = async (req: express.Request, res: express.Response) => {
   try {
-    sendResponse(res, http_status, message, null);
+    const { id } = req.params
+    const { name, description, quantity, unitPrice, store, category, variants } = req.body;
+    const Product = {
+      name: name,
+      description: description,
+      quantity: {
+        available: quantity.available,
+        sold: quantity.sold,
+      },
+      unitPrice: {
+        price: unitPrice.price,
+        unit: unitPrice.unit
+      },
+      store: store,
+      category: category,
+      variants: variants
+    };
+
+    const product = await productModel.findByIdAndUpdate(id, Product, { new: true });
+    if (!product) {
+      throw new HttpError("NotFoundError", `Product with ID '${id}' not found`, 404, null);
+    }
+    sendResponse(res, 200, "Product Updated successfully!", product);
   } catch (error) {
     handleError(error, res);
   }
@@ -67,3 +88,12 @@ export const deleteOne = (req: express.Request, res: express.Response) => {
     handleError(error, res);
   }
 };
+
+
+export const deleteAll = (req: express.Request, res: express.Response) => {
+  try {
+    productModel.find
+  } catch (error) {
+
+  }
+}
